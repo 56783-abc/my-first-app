05737936ab907319140b8cf796b73c8a2e725267
56783-abc <56783@yupparaj.ac.th>
2026-09-07 09:53:31 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -99,3 +99,6 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
+
+st.divider()
+st.write("นางสาววรัทยา สกุณาคีรี เลขที่ 37 ม.4/4")
