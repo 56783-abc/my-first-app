dc3382215a2f6812fd77b87c1ea65240f97e7ceb
56783-abc <56783@yupparaj.ac.th>
2026-09-07 09:35:05 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -10,7 +10,6 @@ if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
 
 
-
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
