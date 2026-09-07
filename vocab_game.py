2c2655b1eb88b8fc7591d48dd37615fd91815dc3
56783-abc <56783@yupparaj.ac.th>
2026-09-07 09:46:09 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -92,12 +92,10 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
     if st.button("📥 ส่งคำตอบ"):
         st.session_state.is_ended = True
         st.rerun()
-time.sleep(1)
+    
+    time.sleep(1)
     st.rerun()
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
-
-st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
