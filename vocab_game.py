a98a2c382c7e785dfe429b5b7bcbaf56049befbf
56783-abc <56783@yupparaj.ac.th>
2026-09-07 09:50:28 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -45,11 +45,11 @@ def show_result_dialog(ans1, ans2):
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
-st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
+    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-if score == 2:
+    if score == 2:
         st.success("🎉 You win!")
-else:
+    else:
         st.error("💀 You lose!")
 
 
