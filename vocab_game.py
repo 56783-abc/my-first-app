e18502003b37abffdae24b310de4844d14c84a0d
56783-abc <56783@yupparaj.ac.th>
2026-09-09 15:59:42 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -100,10 +100,10 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 2: A sweet yellow `m _ n _ o`. 🥭",
+    "ข้อ 3: A sweet yellow `m _ n _ o`. 🥭",
     value=st.session_state.ans3_val,
 )ans4 = st.text_input(
-    "ข้อ 2: A vehicle with four `c _ r`. 🚗",
+    "ข้อ 4: A vehicle with four `c _ r`. 🚗",
     value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
